9bafaa152cf4065b547c9c05a250c2d2647d95cb
Neddymond <Neddymond@gmail.com>
2021-11-30 23:01:42 +0100

    Change post service endpoint

--- posts/index.js
+++ posts/index.js
@@ -11,7 +11,7 @@ const posts = {};
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
